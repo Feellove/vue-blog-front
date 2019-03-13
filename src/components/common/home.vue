8aee700ca4a92b5ce51f8416939b593f6f291524
Feelman <2514259337@qq.com>
2019-03-13 17:43:40 +0800

    界面优化

--- src/components/common/home.vue
+++ src/components/common/home.vue
@@ -1,13 +1,38 @@
 <template>
   <el-container>
     <el-header>
-      <el-row type="flex" class="row-bg" justify="center">
-        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11" justify="center">
-          <a class="page__logo" href="/" title="2019网" alt="2019网">
-            <img src="http://img2.2019w.cn/Rieko_logoW.png" alt="2019网">
+      <el-row
+        type="flex"
+        class="row-bg"
+        justify="center"
+      >
+        <el-col
+          :xs="7"
+          :sm="7"
+          :md="7"
+          :lg="7"
+          :xl="7"
+          justify="center"
+        >
+          <a
+            class="page__logo"
+            href="/"
+            title="2019网"
+            alt="2019网"
+          >
+            <img
+              src="http://img2.2019w.cn/Rieko_logoW.png"
+              alt="2019网"
+            >
           </a>
         </el-col>
-        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+        <el-col
+          :xs="10"
+          :sm="10"
+          :md="10"
+          :lg="10"
+          :xl="10"
+        >
           <el-menu
             default-active="/article"
             background-color="#333"
@@ -27,22 +52,76 @@
             </el-menu-item>
           </el-menu>
         </el-col>
-        <el-col :xs="4" :sm="6" :md="8" :lg="9" :xl="11">
+        <el-col
+          :xs="7"
+          :sm="7"
+          :md="7"
+          :lg="7"
+          :xl="7"
+        >
           <div>
-            <el-input placeholder="请输入内容" prefix-icon="el-icon-search" v-model="input5"></el-input>
+            <el-input
+              placeholder="请输入内容"
+              prefix-icon="el-icon-search"
+              v-model="input5"
+            ></el-input>
           </div>
         </el-col>
       </el-row>
     </el-header>
+    <div class="w-nav">
+      <ul>
+        <li>
+          <a
+            href="/categories/littleTalk/"
+            title=""
+          >微杂谈</a>
+        </li>
+        <li>
+          <a
+            href="/categories/story/"
+            title=""
+          >故事</a>
+        </li>
+        <li>
+          <a
+            href="/categories/historicalCulture/"
+            title=""
+          >历史文化</a>
+        </li>
+        <li>
+          <a
+            href="/categories/movies/"
+            title=""
+          >影视</a>
+        </li>
+        <li>
+          <a
+            href="/categories/game/"
+            title=""
+          >游戏</a>
+        </li>
+        <li>
+          <a
+            href="/categories/technology/"
+            title=""
+          >技术</a>
+        </li>
+        <li class="lastLi">
+          <a
+            href="/categories/share/"
+            title=""
+          >分享</a>
+        </li>
+      </ul>
+    </div>
     <el-main>
       <router-view></router-view>
     </el-main>
     <el-footer>
       <div class="page__container footer__container">
         <p class="footer__copyright">
-          <span
-            style="color:#69747a"
-          >©Rieko © 2018 ,2019w All Rights Reserved.2019网 - 轻阅读 , 看见好时光 苏ICP备17044739号-2</span>
+          <span style="color:#69747a">©Rieko © 2018 ,2019w All Rights Reserved.2019网 - 轻阅读 , 看见好时光 苏ICP备17044739号-2</span>
           <br>
           <span style="color:#69747a">
             本站服务器由
@@ -53,30 +132,54 @@
               title="南京厚知软件有限公司"
             >南京厚知软件有限公司</a> 提供。
           </span>
-          <span id="busuanzi_container_site_pv" style="color: rgb(105, 116, 122); display: inline;">
+          <span
+            id="busuanzi_container_site_pv"
+            style="color: rgb(105, 116, 122); display: inline;"
+          >
             本站总访问量
-            <span style="color:#999;" id="busuanzi_value_site_pv">13013</span> 次--
+            <span
+              style="color:#999;"
+              id="busuanzi_value_site_pv"
+            >13013</span> 次--
           </span>
-          <span id="busuanzi_container_site_uv" style="color: rgb(105, 116, 122); display: inline;">
+          <span
+            id="busuanzi_container_site_uv"
+            style="color: rgb(105, 116, 122); display: inline;"
+          >
             您是本站第
-            <span style="color:#999;" id="busuanzi_value_site_uv">4347</span> 个小伙伴
+            <span
+              style="color:#999;"
+              id="busuanzi_value_site_uv"
+            >4347</span> 个小伙伴
           </span>
         </p>
         <ul class="footer__social-network clearfix">
           <li class="social-network__item">
-            <a href="https://github.com/showcc" target="_blank" title="github">
+            <a
+              href="https://github.com/showcc"
+              target="_blank"
+              title="github"
+            >
               <i class="iconfont icon-github"></i>
             </a>
           </li>
 
           <li class="social-network__item">
-            <a href="https://weibo.com/u/3941909649" target="_blank" title="新浪微博">
+            <a
+              href="https://weibo.com/u/3941909649"
+              target="_blank"
+              title="新浪微博"
+            >
               <i class="iconfont icon-weibo"></i>
             </a>
           </li>
 
           <li class="social-network__item">
-            <a href="https://www.zhihu.com/people/showccc/activities" target="_blank" title="知乎">
+            <a
+              href="https://www.zhihu.com/people/showccc/activities"
+              target="_blank"
+              title="知乎"
+            >
               <i class="iconfont icon-zhihu"></i>
             </a>
           </li>
@@ -113,18 +216,91 @@ export default {
   left: 0;
   z-index: 99;
 }
-
+.el-card__body{
+  padding: 20px 0 20px 20px;
+}
+.el-carousel__container {
+  margin: 10px 0;
+}
+.el-carousel__item {
+  border-radius: 12px;
+  border: 1px solid #efefef;
+  box-shadow: 0px 1px 15px 1px rgba(69, 65, 78, 0.1);
+}
+.el-carousel__button {
+  background-color: #409eff !important;
+  height: 8px !important;
+}
+.w-nav {
+  background: #fff;
+  border-top: 1px solid rgba(255, 255, 255, 0.1);
+  z-index: 99;
+  position: sticky;
+  top: 60px;
+  left: 0px;
+  right: 0px;
+}
+.w-nav > ul {
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  padding: 20px 10px;
+}
+.w-nav ul li {
+  float: left;
+  list-style: none;
+  padding: 0 10px;
+  position: relative;
+  white-space: nowrap;
+}
+.w-nav ul li a {
+  font-size: 14px;
+  letter-spacing: 2px;
+  color: #666;
+  padding: 0 15px 0 5px;
+}
+.w-nav ul li a::after {
+  position: absolute;
+  right: 0;
+  content: "/";
+  color: #999;
+  top: 2px;
+}
 .el-menu {
   border: none !important;
 }
 .el-main {
   padding: 80px 20px 20px;
+  margin: 0 auto;
 }
+@media screen and (min-width: 980px) {
+  .el-main {
+    width: 970px;
+  }
+}
+@media screen and (min-width: 1280px) {
+  .el-main {
+    width: 1280px;
+  }
+}
+/* @media screen and (min-width: 736px) {
+  .el-main {
+  }
+} */
 .page__logo > img {
   height: 40px;
 }
 .el-input--prefix .el-input__inner {
   border-radius: 22px;
+  width: 250px;
+}
+.el-input__icon{
+  font-size: 20px
 }
 .el-footer {
   display: block;
@@ -133,40 +309,40 @@ export default {
   padding: 10px 0;
 }
 .footer__container {
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-pack: justify;
-    justify-content: space-between;
-    -ms-flex-align: center;
-    align-items: center;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-pack: justify;
+  justify-content: space-between;
+  -ms-flex-align: center;
+  align-items: center;
 }
 .page__container {
-    -ms-flex: 1;
-    flex: 1;
-    position: relative;
-    margin: 0 auto;
-    padding: 0 15px;
+  -ms-flex: 1;
+  flex: 1;
+  position: relative;
+  margin: 0 auto;
+  padding: 0 15px;
 }
 .footer__copyright {
-    margin: 0;
-    font-size: 12px;
-    text-align: center;
+  margin: 0;
+  font-size: 12px;
+  text-align: center;
 }
 .footer__social-network {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    font-size: 24px;
-    line-height: 1.2;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  font-size: 24px;
+  line-height: 1.2;
 }
 .footer__social-network .social-network__item {
-    float: left;
-    transition: transform .3s;
+  float: left;
+  transition: transform 0.3s;
 }
 .footer__bottom a {
-    font-size: inherit;
-    text-decoration: none;
-    transition: color .3s;
-    color: #fff;
+  font-size: inherit;
+  text-decoration: none;
+  transition: color 0.3s;
+  color: #fff;
 }
 </style>
--- src/components/compontent/article.vue
+++ src/components/compontent/article.vue
@@ -1,46 +1,175 @@
 <template>
   <div>
-    <ul class="article_box">
-      <li @click="toDetail(article._id)" v-for="(article,index) in articleLists" :key="index">
-        <div class="mini-article__cover">
-          <img itemprop="image" src="http://img.2019w.cn/away1.jpg" alt="姑娘去远方">
-          <div itemprop="datePublished" content class="mini-article__date">
-            <span class="date__day">13</span>
-            <span class="date__month">二月</span>
-          </div>
-        </div>
-        <div class="mini-article__info">
-          <h3 itemprop="name" class="mini-article__title">
-            <a
-              target="_blank"
-              itemprop="url"
-              href="/story/goFarAway.html"
-              title="姑娘去远方"
-            >{{article.articleName}}</a>
-          </h3>
-          <a target="_blank" itemprop="url" href="/story/goFarAway.html" title="姑娘去远方">
-            <p
-              itemprop="articleSection"
-              class="min-article__desc"
-            >姑娘擦去忧伤与彷徨，你的年华山高水长，穿上舞鞋，甩着肩膀，哼着你喜欢的曲子边唱边跳，去你想去的远方</p>
-            <p class="w-Read">
-              <span>阅读全文&gt;</span>
-            </p>
-          </a>
-          <div class="min-article__tags">
-            <i class="iconfont icon-tab"></i>
-            <ul class="tags__list clearfix">
-              <li class="tags__item">
-                <a target="_blank" href="/tags/distance/">远方</a>
-              </li>
-              <li class="tags__item">
-                <a target="_blank" href="/tags/life/">生活</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </li>
-    </ul>
+    <el-carousel
+      :interval="4000"
+      type="card"
+      height="260px"
+    >
+      <el-carousel-item
+        v-for="item in 6"
+        :key="item"
+      >
+        <img
+          src="../../../static/img/blog-bg.jpg"
+          alt=""
+        >
+        <h3>{{item}}</h3>
+      </el-carousel-item>
+    </el-carousel>
+    <div class="content_box">
+      <el-card
+        class="box-card"
+        style="flex:1;margin-right: 20px;"
+      >
+        <h3 class="article_classes">前端</h3>
+        <ul class="article_box">
+          <li
+            @click="toDetail(article._id)"
+            v-for="(article,index) in articleLists"
+            :key="index"
+          >
+            <div class="mini-article__cover">
+              <img
+                itemprop="image"
+                src="http://img.2019w.cn/away1.jpg"
+                alt="姑娘去远方"
+              >
+              <div
+                itemprop="datePublished"
+                content
+                class="mini-article__date"
+              >
+                <span class="date__day">{{article.createTime}}</span>
+                <!-- <span class="date__month">二月</span> -->
+              </div>
+            </div>
+            <div class="mini-article__info">
+              <h3
+                itemprop="name"
+                class="mini-article__title"
+              >
+                <a
+                  target="_blank"
+                  itemprop="url"
+                  href="/story/goFarAway.html"
+                  title="姑娘去远方"
+                >{{article.articleName}}</a>
+              </h3>
+              <a
+                target="_blank"
+                itemprop="url"
+                href="/story/goFarAway.html"
+                title="姑娘去远方"
+              >
+                <p
+                  itemprop="articleSection"
+                  class="min-article__desc"
+                >姑娘擦去忧伤与彷徨，你的年华山高水长，穿上舞鞋，甩着肩膀，哼着你喜欢的曲子边唱边跳，去你想去的远方</p>
+                <p class="w-Read">
+                  <span>阅读全文&gt;</span>
+                </p>
+              </a>
+              <div class="min-article__tags">
+                <i class="el-icon-bell"></i>
+                <ul class="tags__list clearfix">
+                  <li class="tags__item">
+                    <a
+                      target="_blank"
+                      href="/tags/distance/"
+                    >远方</a>
+                  </li>
+                  <li class="tags__item">
+                    <a
+                      target="_blank"
+                      href="/tags/life/"
+                    >生活</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </el-card>
+      <div style="width:280px">
+        <el-card class="box-card">
+          <h3 class="block__title">最新文章</h3>
+          <ul class="block-list latest-post-list">
+            <li class="latest-post-item">
+              <a
+                target="_blank"
+                href="/technology/typeScript.html"
+                title="TypeScript类型"
+              >
+                <div class="item__cover">
+                  <img
+                    src="https://note.youdao.com/yws/api/personal/file/WEB73c4e2c8d530fbc23f28558f99b4c075?method=download&amp;shareKey=7cb1c30b5f968b148541a9193f89211f"
+                    alt="TypeScript类型"
+                  >
+                </div>
+                <div class="item__info">
+                  <h3 class="item__title">TypeScript类型</h3>
+                  <span class="item__text">2019-02-28</span>
+                </div>
+              </a>
+            </li>
+            <li class="latest-post-item">
+              <a
+                target="_blank"
+                href="/story/warm.html"
+                title="愿所有人的未来都温暖"
+              >
+                <div class="item__cover">
+                  <img
+                    src="http://img.2019w.cn/trey-trimble-by-trey-trimble-synth-retrowave-synthwave-new-r.jpg"
+                    alt="愿所有人的未来都温暖"
+                  >
+                </div>
+                <div class="item__info">
+                  <h3 class="item__title">愿所有人的未来都温暖</h3>
+                  <span class="item__text">2019-02-13</span>
+                </div>
+              </a>
+            </li>
+            <li class="latest-post-item">
+              <a
+                target="_blank"
+                href="/story/goFarAway.html"
+                title="姑娘去远方"
+              >
+                <div class="item__cover">
+                  <img
+                    src="http://img.2019w.cn/away1.jpg"
+                    alt="姑娘去远方"
+                  >
+                </div>
+                <div class="item__info">
+                  <h3 class="item__title">姑娘去远方</h3>
+                  <span class="item__text">2019-02-13</span>
+                </div>
+              </a>
+            </li>
+            <li class="latest-post-item">
+              <a
+                target="_blank"
+                href="/story/goFarAway.html"
+                title="姑娘去远方"
+              >
+                <div class="item__cover">
+                  <img
+                    src="http://img.2019w.cn/away1.jpg"
+                    alt="姑娘去远方"
+                  >
+                </div>
+                <div class="item__info">
+                  <h3 class="item__title">姑娘去远方</h3>
+                  <span class="item__text">2019-02-13</span>
+                </div>
+              </a>
+            </li>
+          </ul>
+        </el-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -74,7 +203,7 @@ export default {
             this.articleLists = response.data.message.data;
             this.articleLists.forEach((v, k) => {
               this.articleLists[k].createTime = moment(v.createTime).format(
-                "YYYY-MM-DD HH:mm:ss"
+                "YYYY MM.DD"
               );
             });
             console.log(this.articleLists);
@@ -90,8 +219,84 @@ export default {
   }
 };
 </script>
-
 <style scoped>
+.content_box {
+  display: flex;
+  flex-wrap: wrap;
+}
+.block__title {
+  position: relative;
+  color: #242f35;
+  font-size: 16px;
+  padding-left: 16px;
+  margin: 0px auto 20px;
+  font-weight: 400;
+  height: 20px;
+  line-height: 20px;
+}
+.item__cover {
+  position: relative;
+  display: block;
+  width: 80px;
+  height: 60px;
+  overflow: hidden;
+}
+.item__cover > img {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: block;
+  height: 100%;
+}
+.latest-post-item > a {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  width: 100%;
+  text-decoration: none;
+  color: #242f35;
+}
+.latest-post-item .item__info {
+  display: block;
+  width: calc(100% - 80px);
+  padding: 8px 15px;
+  line-height: 1.4;
+}
+.latest-post-item .item__title {
+  margin: 0;
+  font-weight: 400;
+  font-size: 14px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  transition: color 0.3s;
+}
+.latest-post-item .item__text {
+  font-size: 12px;
+  color: #666;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+.latest-post-item + .latest-post-item {
+  margin-top: 10px;
+}
+.article_classes{
+    font-size: 16px;
+    display: inline-block;
+    padding: 0 20px;
+    font-weight: normal;
+    color: #fff;
+    margin: 0;
+    cursor: pointer;
+    height: 30px;
+    line-height: 30px;
+    background: #409eff;
+    border-radius: 20px;
+    box-shadow: 0 0 10px #409eff;
+}
 .article_box {
   display: flex;
   display: -webkit-flex;
@@ -104,18 +309,29 @@ export default {
   transition: box-shadow 0.3s;
   line-height: 1.4;
   border-radius: 8px;
-  margin-right: 9px;
-  margin-top: 30px;
+  margin: 16px 9px 12px 0;
 }
 .article_box > li:hover {
   box-shadow: 0 0 26px rgba(0, 0, 0, 0.6);
 }
+.el-carousel__item img {
+  width: 100%;
+  object-fit: cover;
+  height: 100%;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 18px;
+  opacity: 0.75;
+  margin: 0;
+}
+
 .mini-article__cover {
   position: relative;
   overflow: hidden;
   padding: 10px 10px 0 10px;
   border-radius: 8px;
-  height: 160px;
+  height: auto;
 }
 .mini-article__cover:hover img {
   transform: scale(1.2);
@@ -124,7 +340,6 @@ export default {
   -moz-transform: scale(1.2);
   -ms-transform: scale(1.2);
 }
-
 .mini-article__cover > img {
   display: block;
   width: 100%;
@@ -157,6 +372,7 @@ export default {
   text-align: center;
   background-color: rgba(64, 84, 90, 0.7);
   line-height: 1;
+  font-size: 14px
 }
 .mini-article__info {
   padding: 15px;
@@ -190,7 +406,8 @@ export default {
 }
 .w-Read {
   font-size: 12px;
-  color: #04ac71;
+  color: #409eff;
+  margin: 12px 0;
 }
 .page__mini-article {
   width: 100%;
@@ -209,3 +426,40 @@ export default {
   transform: translate(-50%, -50%) rotateX(180deg) rotateZ(-360deg) scale(1);
   opacity: 1;
 }
+.sidebar__block {
+  position: relative;
+  display: block;
+  width: 100%;
+}
+.min-article__tags > i {
+  font-size: 20px;
+  vertical-align: middle;
+  margin-right: 5px;
+  color: #409eff;
+}
+.min-article__tags .tags__list {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  vertical-align: middle;
+}
+.min-article__tags .tags__item {
+  float: left;
+}
+.min-article__tags .tags__item + .tags__item::before {
+  content: ", ";
+}
+.min-article__tags .tags__item > a {
+  font-size: 14px;
+  text-decoration: none;
+  transition: color 0.3s;
+  color: #666;
+}
+.min-article__tags .tags__item > a:hover {
+  color: #409eff;
+}
+.latest-post-item .item__title:hover {
+  color: #409eff;
+}
+</style>
